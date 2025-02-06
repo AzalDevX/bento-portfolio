7e3d313531cd0a6a6a80ab39b6f4aedba68bcf12
Gonzalo Azaldegi <98758892+AzalDevX@users.noreply.github.com>
2025-02-06 02:18:16 +0100

    Pasar datos a JSON y remake para template

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        beige: {
+        primary: {
           50: '#fdf8f6',
           100: '#f2e8e5',
           200: '#eaddd7',
@@ -17,7 +17,7 @@ export default {
           800: '#846358',
           900: '#43302b',
         },
-        chocolate: {
+        secondary: {
           50: '#faf5f0',
           100: '#f0e5d8',
           200: '#e1ccb4',
@@ -31,19 +31,19 @@ export default {
         },
       },
       boxShadow: {
-        'beige-sm': '0 1px 2px 0 rgba(191, 160, 148, 0.05)',
-        beige:
+        'primary-sm': '0 1px 2px 0 rgba(191, 160, 148, 0.05)',
+        primary:
           '0 1px 3px 0 rgba(191, 160, 148, 0.1), 0 1px 2px 0 rgba(191, 160, 148, 0.06)',
-        'beige-md':
+        'primary-md':
           '0 4px 6px -1px rgba(191, 160, 148, 0.1), 0 2px 4px -1px rgba(191, 160, 148, 0.06)',
-        'beige-lg':
+        'primary-lg':
           '0 10px 15px -3px rgba(191, 160, 148, 0.1), 0 4px 6px -2px rgba(191, 160, 148, 0.05)',
-        'chocolate-sm': '0 1px 2px 0 rgba(180, 126, 72, 0.05)',
-        chocolate:
+        'secondary-sm': '0 1px 2px 0 rgba(180, 126, 72, 0.05)',
+        secondary:
           '0 1px 3px 0 rgba(180, 126, 72, 0.1), 0 1px 2px 0 rgba(180, 126, 72, 0.06)',
-        'chocolate-md':
+        'secondary-md':
           '0 4px 6px -1px rgba(180, 126, 72, 0.1), 0 2px 4px -1px rgba(180, 126, 72, 0.06)',
-        'chocolate-lg':
+        'secondary-lg':
           '0 10px 15px -3px rgba(180, 126, 72, 0.1), 0 4px 6px -2px rgba(180, 126, 72, 0.05)',
       },
     },
